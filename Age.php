a6d04616592489b84da1cef4ff9e9c7bf2aa8e48
Taity-mini <taitymini@hotmail.co.uk>
2015-10-19 16:03:05 +0100

    Switch cases

---
+++ Age.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1504693
+ * Date: 19/10/2015
+ * Time: 15:41
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Age Activity</title>
+</head>
+<body>
+<p>
+<?php
+$wantedgood = "mugs";
+
+switch ($numberofHobbits) {
+    case "Specs":
+        echo" You have to 16 to buy specs";
+        break;
+    case"mugs":
+        echo"You have to be 18 to buy mugs";
+        break;
+    case "rolls":
+        echo "You have to be 21 to  buy rolls";
+        break;
+    default:
+        echo"All the hobbits have gone home";
+
+}
+
+
+?>
+</p>
+</body>
+</html>
