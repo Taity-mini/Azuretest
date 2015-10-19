da8614048a5c4b2decdcae8440e701e1422f16b1
Taity-mini <taitymini@hotmail.co.uk>
2015-10-19 15:12:52 +0100

    echo with variables

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
 <p>
 <?php
     echo"Hello,"."". "World" . "!";
+    echo 5*7;
+
+    //variables
+    $myname = "Frodo Baggins";
+    $myage = 111;
+
+    echo "My name is" . $myname . "and I am" . $myage;
 ?>
 </p>
 </body>
