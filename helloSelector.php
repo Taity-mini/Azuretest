2a1145a3978cd001d6e6baba09b571560bb29413
Taity-mini <taitymini@hotmail.co.uk>
2015-10-26 16:36:13 +0000

    Lab 3

---
+++ helloSelector.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Hello Selecter</title>
+</head>
+<body>
+<p></p>
+<ul>
+  <li><><a href="./heloPrinter.php?planet=Earth">Earth</a></li>
+  <li>><a href="./heloPrinter.php?planet=Mars">Mars</a></li>
+  <li><a href="./heloPrinter.php?planet=Uranus">Uranus</a></li>
+</ul>
+</body>
+</html>
+
+
