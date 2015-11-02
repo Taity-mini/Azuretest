38e36e9a1d04df27113ea3745c016b0624647fbc
Taity-mini <taitymini@hotmail.co.uk>
2015-11-02 13:42:42 +0000

    Session and cookies Lab

---
+++ homepage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 02/11/2015
+ * Time: 13:28
+ * Home page
+ */
+
+?>
+
+
+
---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 02/11/2015
+ * Time: 13:25
+ */
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+</head>
+<body>
+<H1>Login</H1>
+<form action ="./check_login.php">
+    <label for="Username">Username</label>
+<input type="text" name ="username" title ="Username"/>
+<label for="password" >Password:</label>
+    <input type ="password" name ="pass" title ="Password"/>
+<input type ="submit" name ="submit" value ="Login"/>
+<input type = "reset" value="Reset"/>
+</form>
+</body>
+</html>
