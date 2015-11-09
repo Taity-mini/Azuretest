5843911fe164c0f6b8ed2b7bea1c6dff2fe33f9a
Taity-mini <taitymini@hotmail.co.uk>
2015-11-09 14:36:15 +0000

    Lab 4

---
+++ Lab4/index.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Created by PhpStorm.
+ * User: 1504693
+ * Date: 09/11/2015
+ * Time: 13:59
+ */
+
+//Session Data
+mysql_connect("eu-cdbr-azure-north-d.cloudapp.net", "b49912ac2cf930", "e632d092") or die(mysql_error());
+mysql_select_db("rgu_1504693") or die(mysql_error());
+
+
+
+
+?>
+
+
+
