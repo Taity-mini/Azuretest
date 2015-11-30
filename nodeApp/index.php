c7b2aa00ba8b991a5fe4e6e0440ead0139813c9a
Taity-mini <taitymini@hotmail.co.uk>
2015-11-30 14:20:15 +0000

    Connecting to Twitter via Node Server

---
+++ nodeApp/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1504693
+ * Date: 30/11/2015
+ * Time: 14:11
+ */
+
+?>
+
+<!DocType html>
+<html>
+<head lang ="en">
+    <meta charset ="UTF-8">
+    <title>Comsuming Twitter Webserver</title>
+    <script>
+        window.onload = function() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    document.getElementById("twitter").innerHTML = xhttp.responseText;
+                }
+            };
+            xhttp.open("GET", "http://rgunodeapp.azurewebsites.net/", true);
+            xhttp.send();
+        }
+
+    </script>
+</head>
+<body>
+<header>
+    <h1>Header Div</h1>
+</header>
+<main>
+    <h2>Main content</h2>
+</main>
+<aside id="twitter" >
+
+</aside>
+</body>
+</html>
